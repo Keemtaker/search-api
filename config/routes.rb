72007a544ec3d3a44bf230cea9a5e2695afa34f6
Hakeem Baba <keemtaker@yahoo.com>
2017-11-23 10:49:30 +0100

    created nokogiri

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :api do
+    get "count/:query", to: "pages#count"
+  # end
 end
